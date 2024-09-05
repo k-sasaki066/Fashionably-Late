920b86d947a76dcf631464315df2d8f298f76863
k-sasaki <k.sasaki.bluesky@gmail.com>
2024-09-06 08:30:05 +0900

    css修正

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(CategoriesTableSeeder::class);
-        Contact::factory(5)->create();
+        $this->call(CategoriesTableSeeder::class);
+        Contact::factory(35)->create();
     }
 }
